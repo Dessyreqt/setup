67aaa5155806c1777e149ea9bf7e7226f83e7166
David Carroll <carroll.david.s@gmail.com>
2021-04-19 12:01:43 -0500

    Update powershell profile

--- settings/PowerShell/Microsoft.PowerShell_profile.ps1
+++ settings/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -13,3 +13,18 @@ function CoverFeatures {
 	& "C:\apps\dotCover\dotCover.exe" cover --targetExecutable="C:\Program Files\dotnet\dotnet.exe" --output="C:\CoverageReport\CoverageReport.html" --reportType=HTML --filters="+:type=*.Features.*;-:type=*.Tests.Features.*;-:type=*Controller" -- test $path
 	Invoke-Item "C:\CoverageReport\CoverageReport.html"
 }
+
+# PowerShell parameter completion shim for the dotnet CLI 
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
+Register-ArgumentCompleter -Native -CommandName nuke -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        nuke :complete "$wordToComplete" | ForEach-Object {
+           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
