126e8458d4b06ddc45c77d5fd66b77a8af323218
David Carroll <Dessyreqt@users.noreply.github.com>
2022-03-24 17:58:04 -0500

    Update development applications

--- scripts/DevelopmentApplications.ps1
+++ scripts/DevelopmentApplications.ps1
@@ -3,6 +3,6 @@ choco install -y ilspy
 choco install -y lua53
 choco install -y nodejs.install
 choco install -y python3
-choco install -y visualstudio2019community
+choco install -y visualstudio2022community
 choco install -y vscode
 choco install -y yarn
