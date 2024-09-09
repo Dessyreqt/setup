4fd76ad9ceb937be6814fb7dcc17f9a44bb05e0f
David Carroll <carroll.david.s@gmail.com>
2024-09-09 11:19:30 -0500

    Remove some entries from chocolatey after installing

--- scripts/CommonApplications.ps1
+++ scripts/CommonApplications.ps1
@@ -2,13 +2,16 @@ choco install -y 7zip.install
 choco install -y agentransack
 choco install -y bulkrenameutility.install
 choco install -y chocolateygui
-choco install -y cmdermini
 choco install -y dotnetcore-runtime.install
 choco install -y dotnet-runtime
 choco install -y dotnetfx
 choco install -y dropbox
 choco install -y firefox
+# remove firefox's entry in chocolatey as it autoupdates
+choco uninstall -n -x firefox
 choco install -y googlechrome
+# remove googlechrome's entry in chocolatey as it autoupdates
+choco uninstall -n -x googlechrome
 choco install -y keepassxc
 choco install -y mremoteng
 choco install -y notepadplusplus
