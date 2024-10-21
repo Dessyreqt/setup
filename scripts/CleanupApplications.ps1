3f00c443584b24cd59ce7c17a8fa73d937872985
David Carroll <carroll.david.s@gmail.com>
2024-10-21 12:23:09 -0500

    Move cleanup to separate script

---
+++ scripts/CleanupApplications.ps1
@@ -0,0 +1,5 @@
+# remove entries in chocolatey for apps that autoupdate
+choco uninstall -n -x firefox
+choco uninstall -n -x googlechrome
+choco uninstall -n -x discord
+choco uninstall -n -x spotify
--- scripts/CommonApplications.ps1
+++ scripts/CommonApplications.ps1
@@ -7,11 +7,7 @@ choco install -y dotnet-runtime
 choco install -y dotnetfx
 choco install -y dropbox
 choco install -y firefox
-# remove firefox's entry in chocolatey as it autoupdates
-choco uninstall -n -x firefox
 choco install -y googlechrome
-# remove googlechrome's entry in chocolatey as it autoupdates
-choco uninstall -n -x googlechrome
 choco install -y keepassxc
 choco install -y mremoteng
 choco install -y notepadplusplus
